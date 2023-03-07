6277c26024dbc85927309cde59b0221fc7514b7d
RuslanRymar <rrimar@idiq.com>
2023-03-07 17:57:15 +0400

    revised summary

--- summary.js
+++ summary.js
@@ -11,7 +11,12 @@
         </style>
         <summary>
             <p>
-                Highly skilled and experienced software architect with 10+ years of experience designing and implementing scalable, high-performance software systems for a diverse range of clients. Proficient in a variety of programming languages and platforms, with a strong understanding of software design principles, system architecture, and cloud computing. 
+                Highly skilled and experienced software architect 
+                with 10+ years of experience designing and implementing scalable, 
+                high-performance software systems for a diverse range of clients. 
+            </p>
+            <p>
+                Proficient in a variety of programming languages and platforms, with a strong understanding of software design principles, system architecture, and cloud computing. 
             </p>
             <p>
                 Successfully led cross-functional international teams in delivering high-quality projects within defined timelines and budgets.
